a6dbb8426a10ff187617a113cb8928d92cd2cc20
Lev <Khomchenkov>
2025-03-17 23:32:56 +0700

    feat: add state machine

---
+++ UnityProject/Ecobot/Assets/Project/Scripts/FiniteStateMachine/BaseState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace FiniteStateMachine
+{
+    public abstract class BaseState : IState
+    {
+        public virtual void OnEnter()
+        {
+            // nope
+        }
+
+        public virtual void Update()
+        {
+            // nope
+        }
+
+        public virtual void FixedUpdate()
+        {
+            // nope
+        }
+
+        public virtual void OnExit()
+        {
+            // nope
+        }
+    }
+}
