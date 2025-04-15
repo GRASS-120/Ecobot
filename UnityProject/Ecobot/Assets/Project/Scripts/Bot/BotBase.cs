fa141e0a8530a3b15205b5551ce276ae8297cffb
Lev Khomchenkov <pokemon9116@yandex.ru>
2025-04-15 17:54:15 +0700

    -feat: hz

--- UnityProject/Ecobot/Assets/Project/Scripts/Bot/BotBase.cs
+++ UnityProject/Ecobot/Assets/Project/Scripts/Bot/BotBase.cs
@@ -1,12 +1,13 @@
 using Bot.Command;
 using Grid;
+using InteractionSystem;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Bot
 {
     // todo: добавить FSM? 1. idle, 2. выполняет команду, 3. ожидает и тп...
-    public class BotBase : MonoBehaviour {
+    public class BotBase : MonoBehaviour, IInteractable {
         [Title("Components")]
         [SerializeField] private BotMovementController movementController;
         [SerializeField] private BotCommandController commandController;
@@ -18,6 +19,16 @@ namespace Bot
         {
             movementController.Init(this, gridMap);
             commandController.Init(this);
+        }
+
+        public void StartInteraction(IInteractor interactor)
+        {
+            Debug.Log(interactor);
+        }
+
+        public void StopInteraction(IInteractor interactor)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- UnityProject/Ecobot/Assets/Project/Scripts/Player/PlayerInteractor.cs
+++ UnityProject/Ecobot/Assets/Project/Scripts/Player/PlayerInteractor.cs
@@ -1,5 +1,6 @@
 ﻿using InteractionSystem;
 using Player.InputManager;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 // пока через инпуты, но потом нужно сделать так, чтобы и роботы наверное могли nteract делать... но как?
@@ -8,10 +9,10 @@ namespace Player
 {
     public class PlayerInteractor : MonoBehaviour, IInteractor
     {
-        [Header("Components")]
+        [Title("Components")]
         [SerializeField] private Transform interactorSource;
         
-        [Header("Interaction Params")]
+        [Title("Interaction Params")]
         [SerializeField] private float interactionRange = 10f;
         
         public Transform InteractorSource { get; set; }
@@ -19,7 +20,7 @@ namespace Player
         private PlayerManager _player;
         private PlayerInputManager _input;
         
-        public void Init()
+        private void Awake()
         {
             _player = GetComponent<PlayerManager>();
             _input = _player.Input;
@@ -30,14 +31,11 @@ namespace Player
         
         public void HandleInteractions()
         {
-            Debug.Log("Interact!!!");
-            
             var r = new Ray(InteractorSource.position, InteractorSource.forward);
             if (Physics.Raycast(r, out RaycastHit hit, interactionRange))
             {
                 if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
                 {
-                    Debug.Log("Interact!!!1");
                     interactable.StartInteraction(this);
                 }
             }
